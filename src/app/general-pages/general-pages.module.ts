2325e26f172cf86311e7a2b99d4f7775eb7cf1ee
Home <Home@DESKTOP-OCNR93M>
2021-05-15 21:02:54 +0430

    test

--- src/app/general-pages/general-pages.module.ts
+++ src/app/general-pages/general-pages.module.ts
@@ -8,7 +8,7 @@ const routes : Routes = [
   { path: 'blank-page', component: BlankPageComponent }
 ]
 
-@NgModule({
+@NgModule({ 
   declarations: [BlankPageComponent],
   imports: [
     CommonModule,
