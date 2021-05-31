cb26650f99fc447acbb6f4027cb4b39c4ce4721e
Home <Home@DESKTOP-OCNR93M>
2021-05-31 22:48:49 +0430

    w

--- src/app/site-layout/site-layout.component.ts
+++ src/app/site-layout/site-layout.component.ts
@@ -1,13 +1,13 @@
-import { Component, OnInit } from "@angular/core";
-import { ActivatedRoute } from "@angular/router";
-import * as $ from "jquery";
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import * as $ from 'jquery';
 @Component({
-  selector: "site-layout-root",
-  templateUrl: "./site-layout.component.html",
-  styleUrls: ["./site-layout.component.scss"],
+  selector: 'site-layout-root',
+  templateUrl: './site-layout.component.html',
+  styleUrls: ['./site-layout.component.scss'],
 })
 export class SiteLayoutComponent implements OnInit {
-  menuRoute = "";
+  menuRoute = '';
   constructor(private route: ActivatedRoute) {
     this.menuRoute = route.snapshot.routeConfig.path;
   }
@@ -15,18 +15,18 @@ export class SiteLayoutComponent implements OnInit {
   ngOnInit() {}
   ngAfterViewInit() {
     // loading templates js after dom render
-    $.getScript("../../assets/js/popper.min.js", function () {});
-    $.getScript("../../assets/js/bootstrap.min.js", function () {});
-    $.getScript("../../assets/js/owl.js", function () {});
-    $.getScript("../../assets/js/wow.js", function () {});
-    $.getScript("../../assets/js/validation.js", function () {});
-    $.getScript("../../assets/js/isotope.js", function () {});
-    $.getScript("../../assets/js/appear.js", function () {});
-    $.getScript("../../assets/js/jquery-ui.js", function () {});
-    $.getScript("../../assets/js/jquery.fancybox.js", function () {});
-    $.getScript("../../assets/js/SmoothScroll.js", function () {});
-    $.getScript("../../assets/js/map-helper.js", function () {});
-    $.getScript("../../assets/js/script.js", function () {});
-    $.getScript("../../assets/js/plan_manager.js", function () {});
+    $.getScript('../../assets/js/popper.min.js', function () {});
+    $.getScript('../../assets/js/bootstrap.min.js', function () {});
+    $.getScript('../../assets/js/owl.js', function () {});
+    $.getScript('../../assets/js/wow.js', function () {});
+    $.getScript('../../assets/js/validation.js', function () {});
+    $.getScript('../../assets/js/isotope.js', function () {});
+    $.getScript('../../assets/js/appear.js', function () {});
+    $.getScript('../../assets/js/jquery-ui.js', function () {});
+    $.getScript('../../assets/js/jquery.fancybox.js', function () {});
+    $.getScript('../../assets/js/SmoothScroll.js', function () {});
+    $.getScript('../../assets/js/map-helper.js', function () {});
+    $.getScript('../../assets/js/script.js', function () {});
+    $.getScript('../../assets/js/plan_manager.js', function () {});
   }
 }
