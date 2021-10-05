74a58ada2b2b46178b04ea35fe12d60ef917f9d0
Home <Home@DESKTOP-OCNR93M>
2021-10-05 06:52:02 +0330

    deposit

--- src/app/portal/deposit/deposit.component.ts
+++ src/app/portal/deposit/deposit.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { FormBuilder } from '@angular/forms';
 import { first } from 'rxjs/operators';
 import { AlertService } from 'src/app/_services/alert.service';
@@ -11,17 +11,76 @@ import { WalletService } from 'src/app/_services/wallet.service';
 })
 export class DepositComponent implements OnInit {
   walletList: any;
+  viewChi;
+  constructor(
+    private formBuilder: FormBuilder,
+    private alertService: AlertService,
+    private walletService: WalletService
+  ) {}
+  currentStep = 1;
+  previousButton = 'none';
+  nextButton = 'block';
+  popup = true;
 
-  constructor( private formBuilder: FormBuilder,
-    private alertService:AlertService,
-    private walletService:WalletService) { 
-  }
+  stepOne = 'block';
+  stepTwo = 'none';
+  stepThree = 'none';
+
+  @ViewChild('paymentLi') paymentLi: ElementRef;
+  @ViewChild('transactionIdentificationLi') transactionIdentificationLi: ElementRef;
+  @ViewChild('confirmationLi') confirmationLi: ElementRef;
+
+  @ViewChild('paymentFi') paymentFi: ElementRef;
+  @ViewChild('transactionIdentificationFi') transactionIdentificationFi: ElementRef;
+  @ViewChild('confirmationFi') confirmationFi: ElementRef;
 
   ngOnInit() {
-    this.walletService.getAllForDeposit().pipe(first())
-    .subscribe((response) => {
-      this.walletList = response.data;
-    });
+    this.walletService
+      .getAllForDeposit()
+      .pipe(first())
+      .subscribe((response) => {
+        this.walletList = response.data;
+      });
   }
-
+  previous() {
+    switch (this.currentStep) {
+      case 2:
+        this.paymentLi.nativeElement.className = 'first current';
+        this.transactionIdentificationLi.nativeElement.className = 'disabled';
+        this.previousButton='none';
+        this.stepOne='block';
+        this.stepTwo='none';
+        break;
+      case 3:
+        this.transactionIdentificationLi.nativeElement.className = 'current';
+        this.confirmationLi.nativeElement.className = 'disabled';
+        this.nextButton='block';
+        this.stepTwo='block';
+        this.stepThree='none';
+        break;
+    }
+    --this.currentStep;
+  }
+  next() {
+    switch (this.currentStep) {
+      case 1:
+        this.paymentLi.nativeElement.className = 'first done';
+        this.transactionIdentificationLi.nativeElement.className = 'current';
+        this.previousButton='block';
+        this.stepOne='none';
+        this.stepTwo='block';
+        break;
+      case 2:
+        this.transactionIdentificationLi.nativeElement.className = 'done';
+        this.confirmationLi.nativeElement.className = 'last current';
+        this.nextButton='none';
+        this.stepTwo='none';
+        this.stepThree='block';
+        break;
+    }
+    ++this.currentStep;
+  }
+  closePopup(){
+    this.popup=false;
+  }
 }
