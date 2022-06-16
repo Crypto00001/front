baf747182b0519c353ab74894268a38d4dbe755a
Sasan <sasankarimi92@gmail.com>
2022-06-16 21:08:36 +0430

    some controls added to choose plan

--- src/app/_helpers/jwt.interceptor.ts
+++ src/app/_helpers/jwt.interceptor.ts
@@ -14,7 +14,7 @@ export class JwtInterceptor implements HttpInterceptor {
     intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
         // add auth header with jwt if user is logged in and request is to the api url
         const user = this.accountService.userValue?.data;
-        const isLoggedIn = user && user!==undefined && user.token;
+        const isLoggedIn = user && user.token;
         const isApiUrl = request.url.startsWith(environment.apiUrl);
         if (isLoggedIn && isApiUrl) {
             request = request.clone({
@@ -23,12 +23,11 @@ export class JwtInterceptor implements HttpInterceptor {
                 }
             });
         }
-        else {
+        else if(user === undefined && !request.url.endsWith('login')){
           this.router.navigate(['/login']).then(() => {
             window.location.reload();
           });
         }
-
         return next.handle(request);
     }
 }
--- src/app/portal/choose-plan/choose-plan.component.ts
+++ src/app/portal/choose-plan/choose-plan.component.ts
@@ -1,10 +1,10 @@
-import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { Router } from '@angular/router';
-import { first } from 'rxjs/operators';
-import { AlertService } from 'src/app/_services/alert.service';
-import { UserPlanService } from 'src/app/_services/user-plan.service';
-import { WalletService } from 'src/app/_services/wallet.service';
+import {Component, OnInit} from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {Router} from '@angular/router';
+import {first} from 'rxjs/operators';
+import {AlertService} from 'src/app/_services/alert.service';
+import {UserPlanService} from 'src/app/_services/user-plan.service';
+import {WalletService} from 'src/app/_services/wallet.service';
 
 @Component({
   selector: 'app-choose-plan',
@@ -13,49 +13,62 @@ import { WalletService } from 'src/app/_services/wallet.service';
 })
 export class ChoosePlanComponent implements OnInit {
   walletData: any;
-  planList: Array<any>  = [
-    'Bronze',
-    'Silver',
-    'Gold'
+  planPercentage: any;
+  planMonth: any;
+  planList: Array<any> = [
+    {value: 'Bronze Plan', planDetail: {percentage: 0.25, month: 1}},
+    {value: 'Silver Plan', planDetail: {percentage: 0.35, month: 2}},
+    {value: 'Gold Plan', planDetail: {percentage: 0.5, month: 3}}
   ];
-  cryptoList: Array<any>  = [
-    {key:1,value:'Bitcoin'},
-    {key:2,value:'Etherium'},
-    {key:3,value:'Litecoin'},
-    {key:4,value:'Zcash'},
+  cryptoList: Array<any> = [
+    {key: 1, value: 'Bitcoin'},
+    {key: 2, value: 'Etherium'},
+    {key: 3, value: 'Litecoin'},
+    {key: 4, value: 'Zcash'},
   ];
-  constructor( private formBuilder: FormBuilder,
-    private alertService:AlertService,
-    private userPlanService:UserPlanService,
-    private walletService:WalletService) { 
+
+  constructor(private formBuilder: FormBuilder,
+              private alertService: AlertService,
+              private userPlanService: UserPlanService,
+              private walletService: WalletService) {
   }
+
   popup = false
-  defaultPlan:string = null;
-  balance:string=null;
+  defaultPlan: string = null;
+  balance: string = null;
   form: FormGroup;
   loading = false;
   submitted = false;
+  finalProfit: any;
+
   ngOnInit() {
     this.form = this.formBuilder.group({
       planName: ['', Validators.required],
       walletType: [1, Validators.required],
-      investmentAmount: [ '', Validators.required]
-  });
-  
+      investmentAmount: ['', Validators.required]
+    });
+
   }
-  get f() { return this.form.controls; }
-  async invest(plan:string){
+
+  get f() {
+    return this.form.controls;
+  }
+
+  async invest(plan: any, percentage: any, month: any) {
+    this.finalProfit = 0;
     this.form.controls.planName.setValue(plan);
     this.form.controls.walletType.setValue(1);
     this.form.controls.investmentAmount.setValue('');
-    this.walletData= (await this.walletService.getAll()).data;
-    this.balance = this.walletData.filter(q=>q.walletType==1)[0].availableBalance
-    this.popup=true;
+    this.walletData = (await this.walletService.getAll()).data;
+    this.balance = this.walletData.filter(q => q.walletType === 1)[0].availableBalance
+    this.popup = true;
   }
-  cryptoChange(item){
-    this.balance = this.walletData.filter(q=>q.walletType==item)[0].availableBalance
+
+  cryptoChange(item) {
+    this.balance = this.walletData.filter(q => q.walletType == item)[0].availableBalance
   }
-  onSubmit(){
+
+  onSubmit() {
     this.submitted = true;
 
     // reset alerts on submit
@@ -63,30 +76,41 @@ export class ChoosePlanComponent implements OnInit {
 
     // stop here if form is invalid
     if (this.form.invalid) {
-        return;
+      return;
     }
 
     this.loading = true;
     this.userPlanService.registerPlan(this.form.value)
-        .pipe(first())
-        .subscribe(
-            response => {
-              this.loading = false;
-              this.submitted = false;
-              if(response.hasError)
-              {
-                this.alertService.error(response.errorMessage);
-              }
-              else
-              {
-                this.alertService.success('Investment plan started successfully!', { keepAfterRouteChange: true });
-                this.popup = false;
-              }
-            },
-            () => {
-              this.loading = false;
-              this.submitted = false;
-              this.alertService.error('Something went wrong.');
-            });
+      .pipe(first())
+      .subscribe(
+        response => {
+          this.loading = false;
+          this.submitted = false;
+          if (response.hasError) {
+            this.alertService.error(response.errorMessage);
+          } else {
+            this.alertService.success('Investment plan started successfully!', {keepAfterRouteChange: true});
+            this.popup = false;
+          }
+        },
+        () => {
+          this.loading = false;
+          this.submitted = false;
+          this.alertService.error('Something went wrong.');
+        });
+  }
+
+  onPlanChanged($event: Event) {
+    this.planPercentage = this.planList.find(a => a.value === $event).planDetail.percentage;
+    this.planMonth = this.planList.find(a => a.value === $event).planDetail.month;
+    const investmentValue = Number(this.form.controls.investmentAmount.value);
+    this.finalProfit = parseFloat((investmentValue + (investmentValue * this.planPercentage)).toFixed(8));
+  }
+
+  onBalanceChanged($event: Event) {
+    this.planPercentage = this.planList.find(a => a.value === this.form.controls.planName.value).planDetail.percentage;
+    this.planMonth = this.planList.find(a => a.value === this.form.controls.planName.value).planDetail.month;
+    const investmentValue = Number($event);
+    this.finalProfit = parseFloat((investmentValue + (investmentValue * this.planPercentage)).toFixed(8));
   }
 }
